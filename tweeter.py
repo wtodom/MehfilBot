2b6a4d21ed3afc127fb3a9ad5c51528f7bb1b404
Weston Odom <weston.odom@gmail.com>
2015-01-24 19:41:47 -0800

    logging added for tweeter

--- tweeter.py
+++ tweeter.py
@@ -1,8 +1,20 @@
 import twitter
 import yaml
 
+import logging.config
 
-config = yaml.load(file('mehfilbot.yaml', 'r'))
+
+with open('mehfilbot.yaml', 'r') as main_conf:
+    config = yaml.load(main_conf)
+
+
+with open('logging.yaml', 'r') as f:
+    log_conf = yaml.load(f)
+
+
+logger = logging.getLogger(__name__)
+
+logging.config.dictConfig(log_conf)
 
 api = twitter.Api(
     consumer_key=config['twitter']['api_key'],
@@ -16,14 +28,17 @@ def tweet(content, reply_to_id=None):
     if len(content) > 140:
         spaces = [i for i, ch in enumerate(content) if ch in ' \n\t']
         limit = max(index for index in spaces if index < 140)
+        logger.info('Sending partial tweet: {0}'.format(content[:limit]))
         first_tweet = api.PostUpdate(
             content[:limit],  in_reply_to_status_id=reply_to_id)
         tweet(content[limit:], first_tweet.id)
     else:
+        logger.info('Sending final tweet: {0}'.format(content))
         api.PostUpdate(content,  in_reply_to_status_id=reply_to_id)
 
 
 def tweet_menu(menu):
+    logger.info('Tweeting menu for: {0}'.format(menu['date']))
     menu_items = []
     summary = "Today's Mehfil menu:\n"
     for item in menu.items()[1:]:
@@ -39,8 +54,10 @@ def tweet_menu(menu):
                 item[1]['price']
                 )
             )
+    logger.info('Tweeting summary.')
     summary_tweet = tweet(summary)
     reply_id = summary_tweet.id
     for item in menu_items:
+        logger.info('Tweeting menu item {0}.'.format(item[0]))
         reply = tweet(item, reply_to_id=reply_id)
         reply_id = reply.id
